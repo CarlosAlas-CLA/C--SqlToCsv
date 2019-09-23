a6d50d880eedb7947de242bf8640eb18114e14aa
CarlosAlas <48497546+CarlosAlas-CLA@users.noreply.github.com>
2019-09-23 19:10:13 -0400

    Main

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using CodeLibrary;
+namespace TestCodeLibrary
+    {
+    static class Program
+        {
+        static void Main(string[] args)
+            {
+            string connectionString= "";
+            string query = "select *from dbo.test";
+            string archive = @"";
+            string output = @"";
+            string log = @"";
+            
+            CreateCsvFile.csvFile(connectionString, query, archive, output, log);
+            }
+        }
+    }
