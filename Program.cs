e562fa3e8bcd2ccd22f29fa3d118e3ca2b08e67b
CarlosAlas <48497546+CarlosAlas-CLA@users.noreply.github.com>
2019-09-24 15:41:40 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace TestCodeLibrary
             string fileName = "Daily Invoice -";
             string archive = @"C:\Desktop\New folder\Archive\";
             string output = @"C:Desktop\New folder\Output\";
-            string log = @"C:\\Desktop\New folder\log.txt";
+            string log = @"C:\Desktop\New folder\log.txt";
             
             CreateCsvFile.csvFile(connectionString, query,fileName, archive, output, log);
             }
