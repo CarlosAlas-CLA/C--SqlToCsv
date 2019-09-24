13627a967c33e86837e52698c06b8ba8cbef7bfa
CarlosAlas <48497546+CarlosAlas-CLA@users.noreply.github.com>
2019-09-24 15:41:05 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using CodeLibrary;
+namespace TestCodeLibrary
+    {
+    static class Program
+        {
+        static void Main(string[] args)
+            {
+            string connectionString= " ";
+            string query = "select *from dbo.test";
+            string fileName = "Daily Invoice -";
+            string archive = @"C:\Desktop\New folder\Archive\";
+            string output = @"C:Desktop\New folder\Output\";
+            string log = @"C:\\Desktop\New folder\log.txt";
+            
+            CreateCsvFile.csvFile(connectionString, query,fileName, archive, output, log);
+            }
+        }
+    }
